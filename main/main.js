296552121a7eac5e344dc1564c35ff6de14f2f2e
cxbing1 <983121062@qq.com>
2017-05-29 21:51:41 +0800

    修改

--- main/main.js
+++ main/main.js
@@ -1,3 +1,16 @@
-module.exports = function main() {
-  // Write your cade here
+module.exports = function main(s) {
+  	var num=parseInt(s);
+	var sum=0;
+	while(true)
+	{
+		a=num%10;
+		b=num/10;
+		sum+=a;
+	
+		if(b==0)
+		break;
+	}
+
+
+		return sum;
 };
