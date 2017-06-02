40b370bb9359d9be4aeebe2893c19d0058d3f0b7
cxbing1 <983121062@qq.com>
2017-06-02 19:16:37 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,18 @@
- function main() {
-	var s="123";
+ function main(s) {
+	
   	var num=parseInt(s);
 	var sum=0;
 	while(true)
 	{
 		var a=num%10;
-		num=parseInt（num/10）;
+		num=parseInt（num/10);
 		sum+=a;
 	
 		if(num==0)
 		break;
 	}
 
-		console.log(sum);
+		//console.log(sum);
 		return sum;
 	
 };
